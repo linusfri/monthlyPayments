bab2ab8eccfcb8c02b224ff258765dc0260d2d44
Linus <linus.f@protonmail.ch>
2024-01-21 01:23:25 +0100

    Removed type file, added confirmation when adding user

--- src/data/types/screens/InputSum.ts
+++
@@ -1,8 +0,0 @@
-type InputSum<T> = {
-    setState?: React.Dispatch<React.SetStateAction<T>>,
-    stateUpdateFn?: (entity: T, index: number) => void,
-    atIndex?: number, 
-    entity: T,
-}
-
-export default InputSum;
